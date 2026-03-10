888529d1693e05523ecd21430fd957d8d5fec506
lycqz <lukepson23@gmail.com>
2026-03-10 19:15:12 +0000

    Update SearchBar.js

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,11 +1,13 @@
 import { useState } from "react";
-import { searchMovies } from "../Utils/tmdb";
+import { searchMovies, searchShows } from "../Utils/tmdb";
 
-function SearchBar({ onGuess }) {
+function SearchBar({ onGuess, mode }) {
+
   const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
 
   const handleSearch = async (value) => {
+
     setQuery(value);
 
     if (value.length < 2) {
@@ -13,66 +15,99 @@ function SearchBar({ onGuess }) {
       return;
     }
 
-    const movies = await searchMovies(value);
-    setResults(movies.slice(0, 5));
+    let data = [];
+
+    if (mode === "shows") {
+      data = await searchShows(value);
+    } else {
+      data = await searchMovies(value);
+    }
+
+    setResults(data.slice(0, 5));
+
   };
 
   return (
-    <div style={{marginTop:"15px"}}>
+
+    <div style={{ marginTop: "15px" }}>
 
       <input
         type="text"
-        placeholder="Search for a movie..."
+        placeholder={
+          mode === "shows"
+            ? "Search for a TV show..."
+            : "Search for a movie..."
+        }
         value={query}
         onChange={(e) => handleSearch(e.target.value)}
         style={{
-          padding:"8px",
-          width:"90%",
-          borderRadius:"6px",
-          border:"none"
+          padding: "8px",
+          width: "90%",
+          borderRadius: "6px",
+          border: "none"
         }}
       />
 
       <ul style={{ listStyle: "none", padding: 0, marginTop: "10px" }}>
-        {results.map((movie) => (
-          <li
-            key={movie.id}
-            onClick={() => {
-              onGuess(movie);
-              setQuery("");
-              setResults([]);
-            }}
-            style={{
-              cursor: "pointer",
-              display: "flex",
-              alignItems: "center",
-              gap: "10px",
-              padding: "8px",
-              background: "#f4f4f4",
-              borderRadius: "6px",
-              marginBottom: "6px",
-              color: "black"
-            }}
-          >
 
-            {movie.poster_path && (
-              <img
-                src={`https://image.tmdb.org/t/p/w92${movie.poster_path}`}
-                alt={movie.title}
-                style={{ width: "50px", borderRadius: "4px" }}
-              />
-            )}
+        {results.map((item) => {
 
-            <span>
-              {movie.title} ({movie.release_date?.slice(0, 4)})
-            </span>
+          const title = item.title || item.name;
+          const date = item.release_date || item.first_air_date;
 
-          </li>
-        ))}
+          return (
+
+            <li
+              key={item.id}
+              onClick={() => {
+
+                onGuess(item);
+                setQuery("");
+                setResults([]);
+
+              }}
+              style={{
+                cursor: "pointer",
+                display: "flex",
+                alignItems: "center",
+                gap: "10px",
+                padding: "8px",
+                background: "#f4f4f4",
+                borderRadius: "6px",
+                marginBottom: "6px",
+                color: "black"
+              }}
+            >
+
+              {item.poster_path && (
+
+                <img
+                  src={`https://image.tmdb.org/t/p/w92${item.poster_path}`}
+                  alt={title}
+                  style={{
+                    width: "50px",
+                    borderRadius: "4px"
+                  }}
+                />
+
+              )}
+
+              <span>
+                {title} ({date?.slice(0, 4)})
+              </span>
+
+            </li>
+
+          );
+
+        })}
+
       </ul>
 
     </div>
+
   );
+
 }
 
 export default SearchBar;
